0dbc466b0f12de7f0e26a422b1deb8e6352a51f4
Juri <juri.strumpflohner@gmail.com>
2022-04-06 16:06:02 +0200

    initial setup

---
+++ libs/ngui/src/lib/helloworld/helloworld.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngworkspace-helloworld',
+  templateUrl: './helloworld.component.html',
+  styleUrls: ['./helloworld.component.css'],
+})
+export class HelloworldComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ libs/ngui/src/lib/ngui.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HelloworldComponent } from './helloworld/helloworld.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [HelloworldComponent],
+})
+export class NguiModule {}
